f21e6cab56cc09a08455cb2bcf3334d7215ccb6c
rahul-p19 <pandeyrahul.work@gmail.com>
2025-04-06 10:18:46 +0530

    feat: webrtc updates

--- backend/src/signalling-server/rooms.ts
+++ backend/src/signalling-server/rooms.ts
@@ -1,71 +1,76 @@
 import { Server } from "socket.io";
 import { httpServer } from "..";
 
-const io = new Server(httpServer, {
-  cors: {
-    origin: ["http://localhost:3000", "http://localhost:4173", "https://6znr3gxn-4173.inc1.devtunnels.ms","https://6znr3gxn-3000.inc1.devtunnels.ms"],
-    optionsSuccessStatus: 200,
-    methods: ["GET", "POST"],
-  },
-});
+// const io = new Server(httpServer, {
+// 	cors: {
+// 		origin: [
+// 			"http://localhost:3000",
+// 			"http://localhost:4173",
+// 			"https://6znr3gxn-4173.inc1.devtunnels.ms",
+// 			"https://6znr3gxn-3000.inc1.devtunnels.ms",
+// 		],
+// 		optionsSuccessStatus: 200,
+// 		methods: ["GET", "POST"],
+// 	},
+// });
 
-const rooms: { [roomID: string]: string[] } = {};
-const socketToRoom: { [socketID: string]: string } = {};
+// const rooms: { [roomID: string]: string[] } = {};
+// const socketToRoom: { [socketID: string]: string } = {};
 
-io.on("connection", (socket) => {
-  console.log(`${socket.id} connected`);
+// io.on("connection", (socket) => {
+// 	console.log(`${socket.id} connected`);
 
-  socket.on("joinRoom", (roomID) => {
-    console.log(`${socket.id} joining ${roomID}`);
+// 	socket.on("joinRoom", (roomID) => {
+// 		console.log(`${socket.id} joining ${roomID}`);
 
-    if (rooms[roomID]) {
-      const length = rooms[roomID].length;
-      if (length === 4) {
-        socket.emit("room full");
-        return;
-      }
-      rooms[roomID].push(socket.id);
-    } else {
-      rooms[roomID] = [socket.id];
-    }
-    socketToRoom[socket.id] = roomID;
-    console.log(`Users in room ${roomID}`, rooms[roomID]);
-    const usersInThisRoom = rooms[roomID].filter((id) => id !== socket.id);
+// 		if (rooms[roomID]) {
+// 			const length = rooms[roomID].length;
+// 			if (length === 4) {
+// 				socket.emit("room full");
+// 				return;
+// 			}
+// 			rooms[roomID].push(socket.id);
+// 		} else {
+// 			rooms[roomID] = [socket.id];
+// 		}
+// 		socketToRoom[socket.id] = roomID;
+// 		console.log(`Users in room ${roomID}`, rooms[roomID]);
+// 		const usersInThisRoom = rooms[roomID].filter((id) => id !== socket.id);
 
-    socket.emit("allUsers", usersInThisRoom);
-  });
+// 		socket.emit("allUsers", usersInThisRoom);
+// 	});
 
-  socket.on(
-    "sendSignal",
-    (payload: { signal: any; userToSignal: string; callerID: string }) => {
+// 	socket.on(
+// 		"sendSignal",
+// 		(payload: { signal: any; userToSignal: string; callerID: string }) => {
+// 			console.log(
+// 				`Server: ${payload.callerID} inviting ${payload.userToSignal}`
+// 			);
 
-      console.log(`Server: ${payload.callerID} inviting ${payload.userToSignal}`)
+// 			io.to(payload.userToSignal).emit("userJoined", {
+// 				signal: payload.signal,
+// 				callerID: payload.callerID,
+// 			});
+// 		}
+// 	);
 
-      io.to(payload.userToSignal).emit("userJoined", {
-        signal: payload.signal,
-        callerID: payload.callerID,
-      });
-    }
-  );
+// 	socket.on("returnSignal", (payload) => {
+// 		console.log(`Server: ${socket.id} sending back return signal.`);
 
-  socket.on("returnSignal", (payload) => {
+// 		io.to(payload.callerID).emit("receiveReturnSignal", {
+// 			signal: payload.signal,
+// 			id: socket.id,
+// 		});
+// 	});
 
-    console.log(`Server: ${socket.id} sending back return signal.`)
+// 	socket.on("disconnect", () => {
+// 		const roomID = socketToRoom[socket.id];
+// 		let room = rooms[roomID];
+// 		if (room) {
+// 			room = room.filter((id) => id !== socket.id);
+// 			rooms[roomID] = room;
+// 		}
 
-    io.to(payload.callerID).emit("receiveReturnSignal", {
-      signal: payload.signal,
-      id: socket.id,
-    });
-  });
-
-  socket.on("disconnect", () => {
-    const roomID = socketToRoom[socket.id];
-    let room = rooms[roomID];
-    if (room) {
-      room = room.filter((id) => id !== socket.id);
-      rooms[roomID] = room;
-    }
-
-    socket.broadcast.emit("userDisconnected", socket.id);
-  });
-});
+// 		socket.broadcast.emit("userDisconnected", socket.id);
+// 	});
+// });
--- frontend/src/app/room/[roomId]/page.tsx
+++ frontend/src/app/room/[roomId]/page.tsx
@@ -1,9 +1,11 @@
+"use client";
 import { useEffect, useMemo, useRef, useState } from "react";
 import io from "socket.io-client";
 import Peer, { SignalData } from "simple-peer";
 import { useRouter } from "next/router";
 
 const streamMap = new Map();
+let videoConstraints: { height: number; width: number };
 
 const iceServers = [
 	{ urls: "stun:stun.l.google.com:19302" }, // Public STUN server
@@ -24,27 +26,34 @@ const Video = (props: { peer: Peer.Instance }) => {
 	return <video playsInline autoPlay ref={ref} className="border rounded-lg" />;
 };
 
-const videoConstraints = {
-	height: window.innerHeight / 2,
-	width: (2 * window.innerWidth) / 5,
-};
+// const videoConstraints = {
+// 	height: window.innerHeight / 2,
+// 	width: (2 * window.innerWidth) / 5,
+// };
 
-const Room = () => {
+const Room = ({params}) => {
 	const [peers, setPeers] = useState<Peer.Instance[]>([]);
 	const [sharingScreen, setSharingScreen] = useState<boolean>(false);
 	const [toggleMicText, setToggleMicText] = useState<string>("Turn off mic");
-	const [toggleCameraText, setToggleCameraText] = useState<string>("Turn off camera");
+	// const [roomID, setRoomID] = useState<string>("");
+	const [toggleCameraText, setToggleCameraText] =
+		useState<string>("Turn off camera");
 	const socketRef = useRef<SocketIOClient.Socket>(null);
 	const userVideo = useRef<HTMLVideoElement>(null);
 	const peersRef = useRef<{ peerID: string; peer: Peer.Instance }[]>([]);
 	const currentStreamRef = useRef<MediaStream>(null);
-	const params = useRouter();
-	const roomID = params.query.roomID;
+	// const params = useRouter();
+	const roomID = params.roomID;
 
 	useEffect(() => {
+		videoConstraints = {
+			height: window.innerHeight / 2,
+			width: (2 * window.innerWidth) / 5,
+		};
+
 		const connectToSignallingServer = async () => {
-			// socketRef.current = io.connect("http://localhost:8000");
-			socketRef.current = io.connect("https://6znr3gxn-8000.inc1.devtunnels.ms");
+			socketRef.current = io.connect("http://localhost:2567");
+			// socketRef.current = io.connect("https://6znr3gxn-8000.inc1.devtunnels.ms");
 
 			const stream = await navigator.mediaDevices.getUserMedia({
 				video: videoConstraints,
@@ -257,25 +266,29 @@ const Room = () => {
 			});
 	}
 
-	function toggleMic(){
-		const audioTrack = currentStreamRef.current?.getTracks().find(track => track.kind === 'audio');
-		if(!audioTrack) return;
-		if(audioTrack.enabled){
+	function toggleMic() {
+		const audioTrack = currentStreamRef.current
+			?.getTracks()
+			.find((track) => track.kind === "audio");
+		if (!audioTrack) return;
+		if (audioTrack.enabled) {
 			audioTrack.enabled = false;
 			setToggleMicText("Turn on mic");
-		}else{
+		} else {
 			audioTrack.enabled = true;
 			setToggleMicText("Turn off mic");
 		}
 	}
 
-	function toggleCamera(){
-		const videoTrack = currentStreamRef.current?.getTracks().find(track => track.kind === 'video');
-		if(!videoTrack) return;
-		if(videoTrack.enabled){
+	function toggleCamera() {
+		const videoTrack = currentStreamRef.current
+			?.getTracks()
+			.find((track) => track.kind === "video");
+		if (!videoTrack) return;
+		if (videoTrack.enabled) {
 			videoTrack.enabled = false;
 			setToggleCameraText("Turn on camera");
-		}else{
+		} else {
 			videoTrack.enabled = true;
 			setToggleCameraText("Turn off camera");
 		}
@@ -295,8 +308,16 @@ const Room = () => {
 		<div className="h-screen w-full bg-zinc-900 grid grid-cols-3 p-6 gap-6">
 			<div className="fixed bottom-5 right-5 flex flex-col gap-y-3">
 				<div className="flex flex-col gap-y-2">
-					<button onClick={toggleMic} className="bg-amber-500 rounded-sm text-sm px-4 py-1">{toggleMicText}</button>
-					<button onClick={toggleCamera} className="bg-amber-500 rounded-sm text-sm px-4 py-1">{toggleCameraText}</button>
+					<button
+						onClick={toggleMic}
+						className="bg-amber-500 rounded-sm text-sm px-4 py-1">
+						{toggleMicText}
+					</button>
+					<button
+						onClick={toggleCamera}
+						className="bg-amber-500 rounded-sm text-sm px-4 py-1">
+						{toggleCameraText}
+					</button>
 					{sharingScreen ? (
 						<button
 							className="bg-amber-500 rounded-sm text-sm px-4 py-1"
